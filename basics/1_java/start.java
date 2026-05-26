5bb73b43e24f7a3b553f93d3258cd9ccf8034dd8
Sh4dow_3cho <117041323+Aditya-raj04@users.noreply.github.com>
2026-05-26 18:32:15 +0000

    java basics revisin

---
+++ basics/1_java/start.java
@@ -0,0 +1,136 @@
+// import java.util.*;
+class start{
+    public static void main(String args[]){
+                                // data types //
+        // primitive                                     non-primitive
+        // 1>byte                                        1>string
+        // 2>short                                       2>array
+        // 3>char                                        3>class
+        // 4>boolean                                     4>object
+        // 5>int                                         5>interface
+        // 6>long
+        // 7>float
+        // 8>double
+
+//                               Q > to take a input from user :-
+        // Scanner sc = new Scanner(System.in);
+                                     // String name = sc.nextLine();
+        // String name = sc.next();
+        // float name = sc.nextInt()     // nextInt()
+                                         // nextFloat()
+        // System.out.println(name);
+
+
+//                               Q > To take two input and print their sum;
+        // Scanner num1 = new Scanner(System.in);
+        // Scanner num2 = new Scanner(System.in);
+        // int a  = num1.nextInt();
+        // int b  = num2.nextInt();
+        // int sum = a + b;
+        // System.out.println(sum);
+
+
+//                               Q > to check even or not;
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // if(a%2==0){System.out.println(" a is  even");
+        // }  else {
+        //         System.out.println("odd"); }
+
+
+//                               Q > SWITCH CASE
+            // Scanner sc = new Scanner(System.in);
+            // int a = sc.nextInt();
+    // switch (a){
+    // case 1 : System.out.println("monday");
+    //          break;
+    // case 2 : System.out.println("tuesday");
+    //          break;
+    // case 3 : System.out.println("wednesday");
+    //          break;
+    // case 4 : System.out.println("thursday");
+    //          break;
+    // case 5 : System.out.println("friday");
+    //          break;
+    //          default : System.out.println("sunday");
+    // }
+
+     
+
+
+//                               Q > NEWER VERSION SWITCH CASE                                     
+        //      String day = "monday";
+        //      switch(day){
+        //         case "sunday","saturday" -> System.out.println("6 am");
+        //         // break;
+        //         case "monday" -> System.out.println("10 am");
+        //         // break;
+        //         default -> System.out.println("12 pm");
+        //      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//                               Q > TYPE CASTING
+
+        // byte b = 127;
+        // System.out.println(b);
+
+        // int a  = 257;
+        // byte c =(byte) a;           //type casting to byte format data type
+        // System.out.println(c);  //
+
+        // float f = 24.9f;
+        // int d  =(int)f;
+        // System.out.println(d);   //24
+
+        // byte x = 10;
+        // byte y = 30;
+        // int result = x*y;
+        // System.out.println(result);   //300
+
+        // int a = 5;
+        // int b = 6;
+        // int c = 1;
+        // boolean result;
+        // if(a > b) System.out.println("a is greater");
+        // else if(b > c)System.out.println("b is gereater",&b);
+        // else if(c > a)System.out.println("c is greater",&c);
+
+
+    
+        // int n = 5;
+        // int result = 0;
+        // result = n % 2 == 0 ? 10 : 20;
+
+        int a =30;
+        // double a = 40.44;
+        double b = a;             //implicit casting
+
+
+        System.out.println(b);
+
+        char gradeLevel = 'A';
+System.out.println(gradeLevel);
+//You can also assign using Unicode:
+char symbol = '\u2605';
+System.out.println(symbol);
+//Default value: '\u0000'
+        
+
+
+    }
+}
+
+
+
